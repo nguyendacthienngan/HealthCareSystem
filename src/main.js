244b56fc317182296ceac262aaaa4a9f4e29cd70
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-08-14 02:11:39 +0700

    Adding dialogflow api v2

--- src/main.js
+++ src/main.js
@@ -6,9 +6,42 @@ import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 
+import config from './config'
+import translations from './translations/translations.json'
+import { register_service_worker } from './utils'
+
+register_service_worker()
+
+Vue.config.productionTip = false
+Vue.prototype.config = config // <- set config to global scope
+Vue.prototype.translations = translations // <- set translations to global scope
+
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 Vue.prototype.$log = console.log.bind(console)
+
+/* (global) This code is going to tell us, if history mode can be activated on the client, so the application can be consumed without sessionStorage */
+Vue.prototype.history = () => {
+  try {
+      sessionStorage.getItem('check')
+      return true
+  }
+
+  catch {
+      return false
+  }
+}
+
+
+/* (global) Currently selected language or fallback language (en). Needs to be a function, since it's reactive. No need for vuex there */
+Vue.prototype.lang = () => {
+  if (Vue.prototype.history()) return sessionStorage.getItem('lang') || config.fallback_lang
+  return config.fallback_lang
+}
+
+/* (global) Debug mode */
+Vue.prototype.debug = () => process.env.NODE_ENV == 'development'
+
 
 new Vue({
   el: '#app',
--- src/views/videocall/VideoCall.vue
+++ src/views/videocall/VideoCall.vue
@@ -7,13 +7,6 @@
 <script>
 export default {
   mounted() {
-    // const callObject = window.DailyIframe.createCallObject();
-    // callObject.on('participant-updated', (event) => {
-    //     const p = event.participant;
-    //     myCustomMediaStreamTracksHandler(p.user_id, p.videoTrack, p.audioTrack);
-    // });
-    // callObject.join({ url: 'https://ehealthcaresystem.daily.co/Test' });
-
     var callFrame = window.DailyIframe.createFrame({
         showLeaveButton: true,
         iframeStyle: {
@@ -22,7 +15,6 @@ export default {
             height: '100%'
         }
     });
-    // var callFrame = window.DailyIframe.createFrame();
     callFrame.join({ url: "https://ehealthcaresystem.daily.co/Test" });
   },
 };
