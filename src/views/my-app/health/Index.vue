654865f6677d7caca5da0e149d4044976afc2888
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-08-15 03:46:22 +0700

    Adding WidgetDropdown

--- src/views/my-app/health/Index.vue
+++ src/views/my-app/health/Index.vue
@@ -1,3 +1,107 @@
 <template>
-    <h1>Hi</h1>
+  <CRow>
+    <CCol sm="12" lg="4">
+      <CWidgetDropdown color="primary" header="9.823" text="Members online">
+        <template #default>
+          <CDropdown
+            color="transparent p-0"
+            placement="bottom-end"
+          >
+            <template #toggler-content>
+              <CIcon name="cil-settings"/>
+            </template>
+            <CDropdownItem>Action</CDropdownItem>
+            <CDropdownItem>Another action</CDropdownItem>
+            <CDropdownItem>Something else here...</CDropdownItem>
+            <CDropdownItem disabled>Disabled action</CDropdownItem>
+          </CDropdown>
+        </template>
+        <template #footer>
+          <CChartLineSimple
+            pointed
+            class="mt-3 mx-3"
+            style="height:70px"
+            :data-points="[65, 59, 84, 84, 51, 55, 40]"
+            point-hover-background-color="primary"
+            label="Members"
+            labels="months"
+          />
+        </template>
+      </CWidgetDropdown>
+    </CCol>
+    <CCol sm="12" lg="4">
+      <CWidgetDropdown color="info" header="9.823" text="Members online">
+        <template #default>
+          <CDropdown
+            color="transparent p-0"
+            placement="bottom-end"
+            :caret="false"
+          >
+            <template #toggler-content>
+              <CIcon name="cil-location-pin"/>
+            </template>
+            <CDropdownItem>Action</CDropdownItem>
+            <CDropdownItem>Another action</CDropdownItem>
+            <CDropdownItem>Something else here...</CDropdownItem>
+            <CDropdownItem disabled>Disabled action</CDropdownItem>
+          </CDropdown>
+        </template>
+        <template #footer>
+          <CChartLineSimple
+            pointed
+            class="mt-3 mx-3"
+            style="height:70px"
+            :data-points="[1, 18, 9, 17, 34, 22, 11]"
+            point-hover-background-color="info"
+            :options="{ elements: { line: { tension: 0.00001 }}}"
+            label="Members"
+            labels="months"
+          />
+        </template>
+      </CWidgetDropdown>
+    </CCol>
+    <CCol sm="12" lg="4">
+      <CWidgetDropdown
+        color="warning"
+        header="9.823"
+        text="Members online"
+      >
+        <template #default>
+          <CDropdown
+            color="transparent p-0"
+            placement="bottom-end"
+          >
+            <template #toggler-content>
+              <CIcon name="cil-settings"/>
+            </template>
+            <CDropdownItem>Action</CDropdownItem>
+            <CDropdownItem>Another action</CDropdownItem>
+            <CDropdownItem>Something else here...</CDropdownItem>
+            <CDropdownItem disabled>Disabled action</CDropdownItem>
+          </CDropdown>
+        </template>
+        <template #footer>
+          <CChartLineSimple
+            class="mt-3"
+            style="height:70px"
+            background-color="rgba(255,255,255,.2)"
+            :data-points="[78, 81, 80, 45, 34, 12, 40]"
+            :options="{ elements: { line: { borderWidth: 2.5 }}}"
+            point-hover-background-color="warning"
+            label="Members"
+            labels="months"
+          />
+        </template>
+      </CWidgetDropdown>
+    </CCol>
+  </CRow>
 </template>
+
+<script>
+import { CChartLineSimple, CChartBarSimple } from '../../charts/index.js'
+
+export default {
+  name: 'WidgetsDropdown',
+  components: { CChartLineSimple, CChartBarSimple }
+}
+</script>
