975cb81bd079fdd76180c33a25f7ef82597c16ec
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-08-15 03:03:03 +0700

    Adding create schedule

--- src/views/my-app/meetings/Index.vue
+++ src/views/my-app/meetings/Index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div id='app'>
-  <ejs-schedule height='550px' width='100%' :selectedDate='selectedDate'
-  :eventSettings= 'eventSettings'>
-    <e-views>
-      <e-view option='Week' startHour='07:00' endHour='15:00' ></e-view>
-      <e-view option='WorkWeek' startHour='10:00' endHour='18:00' ></e-view>
-      <e-view option='Month' showWeekend=false></e-view>
-    </e-views>
-  </ejs-schedule>
+  <div id=''>
+    <div class="card-header-actions p-3 ">
+        <CButton type="submit" size="sm" color="primary" class="" href="#/meetings/create">
+          <CIcon :content="$options.freeSet.cilPlus"/> 
+          Create
+        </CButton>
+    </div>
+    <ejs-schedule height='550px' width='100%' :selectedDate='selectedDate'
+    :eventSettings= 'eventSettings'>
+      <e-views>
+        <e-view option='Week' startHour='07:00' endHour='15:00' ></e-view>
+        <e-view option='WorkWeek' startHour='10:00' endHour='18:00' ></e-view>
+        <e-view option='Month' showWeekend=false></e-view>
+      </e-views>
+    </ejs-schedule>
   </div>
 </template>
 <script>
@@ -15,16 +21,19 @@
   import { scheduleData } from './datasource.js';
   import { extend } from '@syncfusion/ej2-base';
   import { SchedulePlugin,  WorkWeek, Month, Week } from '@syncfusion/ej2-vue-schedule';
+  import { freeSet } from '@coreui/icons'
+
   Vue.use(SchedulePlugin);
   export default {
-data () {
-  return {
-    eventSettings: { dataSource: extend([], scheduleData, null, true) },
-    selectedDate: new Date(2018, 1, 15)
-  }
-},
-provide: {
-  schedule: [ WorkWeek, Month, Week]
-}
+    freeSet,
+    data () {
+      return {
+        eventSettings: { dataSource: extend([], scheduleData, null, true) },
+        selectedDate: new Date(2018, 1, 15)
+      }
+    },
+    provide: {
+      schedule: [ WorkWeek, Month, Week]
+    }
   }
 </script>
--- src/views/my-app/meetings/New.vue
+++ src/views/my-app/meetings/New.vue
@@ -1,3 +1,20 @@
 <template>
-    
+      <Datepicker 
+        v-model="this.date"
+        :lang="fr" 
+      />
+    <!-- <div></div> -->
+
 </template>
+<script>
+  import VueDatepickerUi from 'vue-datepicker-ui';
+
+  import 'vue-datepicker-ui/lib/vuedatepickerui.css';
+
+  export default {
+    components: {
+      Datepicker: VueDatepickerUi
+    },
+    date: new Date()
+  }
+</script>
